0dc4a58fc7c5e6b4d1565de40184eb4e50e8f05f
NikitaShevnin <mrborman95@gmail.com>
2022-12-11 23:27:45 +0300

    download

--- 01/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа 
-
-System.Console.WriteLine("Введите число");
-string? s = Console.ReadLine();
-int a=Convert.ToInt32(s);
-int b=a*a;
-System.Console.WriteLine(b);
