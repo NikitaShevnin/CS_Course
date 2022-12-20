a0f966e72f6e5e7db56a737bacecc6744d6328cf
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 22:48:30 +0300

    added

---
+++ sem07/t71/natural_numbers_revers.cs
@@ -0,0 +1,20 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+int N = ReadInt("Задайте число N: ");
+Number(N);
+
+int ReadInt(string m)
+{
+    System.Console.Write(m);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Number(int N)
+{   
+    if (N < 1)
+    {
+        return;
+    }
+    Console.Write($"{N, 5}");
+    Number(N - 1);
+}
