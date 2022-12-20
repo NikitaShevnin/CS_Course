fceb1f65c3a5ed98b068ce55ded24a5fcc73cb4c
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:29:17 +0300

    all_save

---
+++ Sem02/20_sem2/20_Program.cs
@@ -0,0 +1,12 @@
+﻿// 20.	Определить номер четверти плоскости, в которой находится точка с координатами Х и У,
+// причем X ≠ 0 и Y ≠ 0
+
+System.Console.WriteLine("введите координату X");
+int x=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите координату Y");
+int y=Convert.ToInt32(Console.ReadLine());
+
+if(x>0 && y>0) System.Console.WriteLine("первая четверть плоскости");
+if(x<0 && y>0) System.Console.WriteLine("Вторая четверть плоскости");
+if(x<0 && y<0) System.Console.WriteLine("Третья четверть плоскости");
+if(x>0 && y<0) System.Console.WriteLine("Четвертая четверть плоскости");
