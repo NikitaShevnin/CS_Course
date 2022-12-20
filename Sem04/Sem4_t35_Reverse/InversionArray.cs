94117a033155a08fdc3514d349324272516d34eb
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:54:08 +0300

    added

---
+++ Sem04/Sem4_t35_Reverse/InversionArray.cs
@@ -0,0 +1,17 @@
+// Написать программу замены элементов массива на противоположные
+
+int[] numbers = { -4, -3, -2, -1,0, 1, 2, 3, 4 };
+             
+int n = numbers.Length;
+int k = n / 2;
+int temp;
+for(int i=0; i < k; i++)
+{
+    temp = numbers[i];
+    numbers[i] = numbers[n - i - 1];
+    numbers[n - i - 1] = temp;
+}
+foreach(int i in numbers)
+{
+    Console.Write($"{i} \t");
+}
---
+++ Sem04/Sem4_t35_Reverse/ReverseProgram.cs
@@ -0,0 +1,7 @@
+﻿// вывод массива в обратном порядке
+int[] myArray = {1,2,3,4,5,6,7,8,9};
+
+for (int i = myArray.Length-1; i>0; i--)
+{
+    System.Console.Write(myArray[i]);
+}
