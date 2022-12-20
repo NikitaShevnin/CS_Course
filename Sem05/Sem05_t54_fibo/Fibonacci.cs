20c8fcf43d092e9a09b51a3aa0067758dac879dd
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 22:05:57 +0300

    add

---
+++ Sem05/Sem05_t54_fibo/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿
+// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+int N = ReadInt("Введите число N: ");
+int[] a = new int[N];
+a[0] = 0;
+a[1] = 1;
+
+for (int i=2; i<N; i++)
+{
+    a[i] = a[i-1] + a[i-2];
+}
+
+PrintArray(a);
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i], 5}");
+    }
+}
