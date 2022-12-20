b2e32b29853b971255338a20499dfaa84415a894
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:45:47 +0300

    added

---
+++ Sem03/30_Sem3_Multiplication/Multiplication_Program.cs
@@ -0,0 +1,18 @@
+﻿// 30. Написать программу вычисления произведения чисел от 1 до N
+
+  Console.Write("Введите число : ");
+  int num = int.Parse(Console.ReadLine());
+  int factorial=1;
+  for(int i=1; i<=num; i++) 
+  {
+        factorial*=i;
+        if(i==num) 
+        {
+            Console.Write("{0}", i);
+        }
+        else 
+        {
+            Console.Write("{0} * ", i);
+        }
+  }
+  Console.Write(" = {0}", factorial);
