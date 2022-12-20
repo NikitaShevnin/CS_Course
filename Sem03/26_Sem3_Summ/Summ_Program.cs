b6c50295eed77dd78d6e02c86bec9ab682fa6bcb
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:41:36 +0300

    added

---
+++ Sem03/26_Sem3_Summ/Summ_Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите диапазон для расчета");
+int a=Convert.ToInt32(System.Console.ReadLine());
+int n=1;
+int sum = 0;
+
+bool Check(int n)
+{
+if (n<=a) return true; else return false;
+}
+
+while (Check (n))
+{
+sum=sum+n;
+n++;
+}
+Console.WriteLine(sum);
