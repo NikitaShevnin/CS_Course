a21351f793b6ee5c770bb2f32c1d9710520750f3
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 22:50:45 +0300

    added

---
+++ sem07/t75/Ackerman.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу вычисления функции Аккермана
+
+System.Console.WriteLine("Найдем функции Аккермана A(m,n)");
+int m = ReadInt("Введите значение m: ");
+int n = ReadInt("Введите значение n: ");
+
+System.Console.WriteLine(Accrement(m, n));
+
+
+int ReadInt(string m)
+{
+    System.Console.Write(m);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Accrement(int m, int n)
+{
+    if (m == 0)
+    {
+        return n+1;
+    } else if (n == 0)
+    {
+        return Accrement(m-1, 1);
+    } else
+    {
+        return Accrement(m-1, Accrement(m, n-1));
+    }
+}
