c3051ae49ff7bf1df790d8edc8341b6992f62c85
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 22:46:09 +0300

    added

---
+++ sem07/t70/natural_numbers.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+int N = ReadInt("Задайте число N: ");
+Number(N, 1);
+
+int ReadInt(string m)
+{
+    System.Console.Write(m);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Number(int n, int i)
+{
+    if (i <= n)
+    {
+        System.Console.Write($"{i, 5}");
+        Number(N, i+1);
+    }
+}
