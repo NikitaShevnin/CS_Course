13a59b82bd0d96e41c3aa7749a11fde7eab1848a
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:39:46 +0300

    add

---
+++ Sem03/25_Sem3_Cube/Cube_Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+
+System.Console.WriteLine("Введите диапазон выводимых чисел");
+int n=Convert.ToInt32(System.Console.ReadLine());
+int a=1;
+int stepen=3;
+
+bool Check(int a)
+{
+    if (a<=n) return true; else return false;
+}
+
+Console.WriteLine("Число" + "\t" + "Квадрат числа");
+while (Check (a))
+    {
+     Console.WriteLine(a + "\t" + a*a*a);
+     a++;
+    }
