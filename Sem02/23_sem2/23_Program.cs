7f6eb57718b97e28ca891cc63c41744d3669857c
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:32:48 +0300

    added

---
+++ Sem02/23_sem2/23_Program.cs
@@ -0,0 +1,10 @@
+﻿// 23.	Найти расстояние между точками в пространстве 2D
+
+double x1,y1,x2,y2,distance;
+double x1=Convert.ToDouble(System.Console.WriteLine());
+double y1=Convert.ToDouble(System.Console.WriteLine());
+double x2=Convert.ToDouble(System.Console.WriteLine());
+double y2=Convert.ToDouble(System.Console.WriteLine());
+
+distance=Math.sqrt((x2-x2)*(x2-x2)+Math.pow(y2-y1,2));
+System.Console.WriteLine(distance);
