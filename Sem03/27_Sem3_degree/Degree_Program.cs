eacb69270885eb89341f7ec672fb634cb880e842
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:43:30 +0300

    added

---
+++ Sem03/27_Sem3_degree/Degree_Program.cs
@@ -0,0 +1,9 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("введите значение A");
+double a=Convert.ToDouble(System.Console.ReadLine());
+Console.WriteLine("Напишите в какую степень возводим значение А?");
+double b=Convert.ToDouble(System.Console.ReadLine());
+
+if(a>0)
+Console.WriteLine("a в степени b=" + Math.Pow(a,b));
+else System.Console.WriteLine("0 нельзя возвести в степень");
