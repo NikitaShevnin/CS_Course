16fbd1b2377076d7bc05bdcd31f5373397baec3b
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 21:46:45 +0300

    added

---
+++ Sem03/31_Sem3_Random/Program_Random.cs
@@ -0,0 +1,17 @@
+﻿// Сгенерировать 10 случайныйх чисел, показать кубы тех чисел которые заканчиваются
+// на четную цифру
+
+bool test (int a) 
+{
+ return a*a*a%10%2==0;
+}
+
+
+Random random=new Random(10);
+for (int i=0;i<20;i++)
+{
+ int a=random.Next(1,20);
+ int b=a;
+ if (test (b))
+    System.Console.WriteLine($"{a}, {b}");
+}
