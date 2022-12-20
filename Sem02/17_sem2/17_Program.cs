f7f5be20897c597f42457d6341c575de9f45986a
NikitaShevnin <mrborman95@gmail.com>
2022-12-20 20:57:53 +0300

    null

---
+++ Sem02/17_sem2/17_Program.cs
@@ -0,0 +1,13 @@
+﻿// 17.	Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+
+System.Console.WriteLine("введите число обозначающее день недели");
+int n=Convert.ToInt32(Console.ReadLine());
+int Mon=1;
+int Tue=2;
+int Wed=3;
+int Thur=4;
+int fri=5;
+int sat=6;
+int sun=7;
+    if (n==sat || n==sun)System.Console.WriteLine("это выходной день");
+    else System.Console.WriteLine("Это будний день");
--- chernovik/Chernovik.cs
+++ chernovik/Chernovik.cs
@@ -1,16 +1,1 @@
 ﻿// Черновик
-
-Console.Write("Введите числа через пробел: ");
-int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-
-int Numbers(int[] array)
-{
-  int count = 0;
-
-  for (int i = 0; i < array.Length; i++)
-  {
-    if (array[i] > 0) count++;
-  }
-  return count;
-}
-Console.WriteLine(Numbers(array));
