8f97e0b631fc45a0e52a41f0848e961a3e8457da
NikitaShevnin <mrborman95@gmail.com>
2022-12-26 22:39:19 +0300

    add_all

---
+++ task84/proper_record.cs
@@ -0,0 +1,17 @@
+﻿// Определить являются ли введенные с клавиатуры символы правильной
+// записью целого числа. Вычислить сумму цифр введенного числа
+
+string number = Console.ReadLine();
+int t = 0;
+if (!Int32.TryParse(number, out t))
+{
+    System.Console.WriteLine("Not number");
+} else
+{
+    int sum = 0;
+    foreach (char d in number)
+    {
+        sum += d-48;
+    }
+    System.Console.WriteLine(sum);
+}
