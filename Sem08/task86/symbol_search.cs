598e06b4ccff418a4051193bc538afe292d36e59
NikitaShevnin <mrborman95@gmail.com>
2022-12-26 22:33:44 +0300

    done

---
+++ Sem08/task86/symbol_search.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сколько раз определенный символ (например 'a') встречается в строке
+
+string s = Console.ReadLine();
+char c = 'a';
+int howMach = 0;
+foreach (char i in s)
+{
+    if (i == c)
+    {
+        howMach ++;
+    }
+}
+System.Console.WriteLine(howMach);
