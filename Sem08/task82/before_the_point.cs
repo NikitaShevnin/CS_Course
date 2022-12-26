6f66a9fc4b4c880a30535d2c61b7df51434fc8a2
NikitaShevnin <mrborman95@gmail.com>
2022-12-26 21:40:04 +0300

    done

---
+++ Sem08/task82/before_the_point.cs
@@ -0,0 +1,7 @@
+﻿// C клавиатуры вводится строка разделенная точкой. Подсчитать количество 
+// символов до точки
+
+string s = Console.ReadLine();
+char ch = '.';
+int indexOfChar = s.IndexOf(ch);
+System.Console.WriteLine(indexOfChar);
